395096d3a03743edb1dde0c030940d88a4d4666b
go-web5 <go.web.creator@gmail.com>
2024-06-01 21:13:53 +0900

    コメントアウト追加

--- app/api/item/create/route.js
+++ app/api/item/create/route.js
@@ -6,8 +6,14 @@ export async function POST(request) {
   const reqBody = await request.json()
 
   try {
+    // MongoDB データベースへの接続を管理するための関数
+    // 接続が成功したか失敗したかをログに記録
     await connectDB() 
+
+    // データベースへの書き込みする、引数に書き込みたいデータを入れる
     await ItemModel.create(reqBody)
+
+    // NextResponse は Next.js だけで使える
     return NextResponse.json({message: "アイテム作成成功"})
 
   } catch(err) {
--- app/utils/schemaModels.js
+++ app/utils/schemaModels.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 
+// 保存するデータの形と種類を定義
 const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
@@ -10,5 +11,5 @@ const ItemSchema = new Schema({
   email: String,
 })
 
-// データベース操作を実行するには Model が必要
+// データベース操作を実行するには Model が必要。Model は Schemaをベースに生成する
 export const ItemModel = mongoose.model.Item || mongoose.model("Item", ItemSchema)
