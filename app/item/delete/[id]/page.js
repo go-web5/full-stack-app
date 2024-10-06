db860962bb85a40aa5265a4e754ee493841c4d4a
go-web5 <go.web.creator@gmail.com>
2024-10-07 08:22:18 +0900

    編集＆削除ページに「権限がありません」が一瞬表示さていた為、ローディング画面追加

--- app/item/delete/[id]/page.js
+++ app/item/delete/[id]/page.js
@@ -11,6 +11,7 @@ const DeleteItem = (context) => {
   const [image, setImage] = useState("")
   const [description, setDescription] = useState("")
   const [email, setEmail] = useState("")
+  const [loding, setLoding] = useState(false)
 
   const loginUserEmail = useAuth()
 　const router = useRouter()
@@ -28,6 +29,7 @@ const DeleteItem = (context) => {
       setImage(singleItem.image)
       setDescription(singleItem.description)
       setEmail(singleItem.email)
+      setLoding(true)
     }
 
     getSingleItem(context.params.id)
@@ -60,21 +62,25 @@ const DeleteItem = (context) => {
     }
   }
 
-  if(loginUserEmail === email) {
-    return (
-      <div>
-        <h1 className="page-title">アイテム削除</h1>
-        <form onSubmit={handleSubmit}>
-          <h2>{title}</h2>
-          <Image src={image} width={750} height={500} alt="item-image" priority/>
-          <h3>¥{price}</h3>
-          <p>{description}</p>
-          <button>削除</button>
-        </form>
-      </div>
-    )
+  if(loding) {
+    if(loginUserEmail === email) {
+      return (
+        <div>
+          <h1 className="page-title">アイテム削除</h1>
+          <form onSubmit={handleSubmit}>
+            <h2>{title}</h2>
+            <Image src={image} width={750} height={500} alt="item-image" priority/>
+            <h3>¥{price}</h3>
+            <p>{description}</p>
+            <button>削除</button>
+          </form>
+        </div>
+      )
+    } else {
+      return <h1>権限がありません</h1>
+    }
   } else {
-    return <h1>権限がありません</h1>
+    return <h1>ローディング中...</h1>
   }
 }
 
