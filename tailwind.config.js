3d39c471cd1f015e13058ab5e43bdbb0740b3179
go-web5 <go.web.creator@gmail.com>
2024-10-09 21:01:05 +0900

    Tailwind CSS関連をインストール、ファイル追加など

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './item/**/*.{js,ts,jsx,tsx,mdx}',
+    './user/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
