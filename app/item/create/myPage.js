8189b023161caf2ee64f6fdd56ca9747ce21aedb
go-web5 <go.web.creator@gmail.com>
2025-02-12 22:21:31 +0900

    アイテム作成と編集ページにラベルと必須項目マーク追加、価格のtypeをnumberに変更、それに伴うスタイル調整

--- app/item/create/myPage.js
+++ app/item/create/myPage.js
@@ -47,14 +47,26 @@ const CreateItem = () => {
     return (
       <main>
         <h1 className="lower-header">アイテム作成</h1>
-        <div className="mb-6">
-          <ImgInput setImage={setImage}/>
-        </div>
         <form onSubmit={handleSubmit}>
-          <input className="user-input" value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="アイテム名" required/>
-          <input className="user-input" value={price} onChange={(e) => setPrice(e.target.value)} type="text" name="price" placeholder="価格" required/>
-          <input className="user-input" value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required/>
-          <textarea className="user-textarea" value={description} onChange={(e) => setDescription(e.target.value)} name="description" rows={10} placeholder="商品説明" required></textarea>
+          <label className="user-label">
+            <span>アイテム名</span> <span className="text-red-500">＊</span>
+            <input className="user-input" value={title} onChange={(e) => setTitle(e.target.value)} maxLength={50} type="text" name="title" placeholder="アイテム名" required/>
+          </label>
+          <label className="user-label">
+            <span>価格</span> <span className="text-red-500">＊</span>
+            <input className="user-input" value={price} onChange={(e) => setPrice(e.target.value)} type="number" name="price" placeholder="価格" inputMode="numeric" required/>
+          </label>
+          <label className="user-label">
+            <span>画像</span> <span className="text-red-500">＊</span>
+            <div className="mt-1 mb-1">
+              <ImgInput setImage={setImage}/>
+            </div>
+            <input className="user-input" value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required/>
+          </label>
+          <label className="user-label">
+            <span>商品説明</span> <span className="text-red-500">＊</span>
+            <textarea className="user-textarea" value={description} onChange={(e) => setDescription(e.target.value)} name="description" rows={10} placeholder="商品説明" required></textarea>
+          </label>
           <button className="btn-secondary">作成</button>
         </form>
       </main>
